bbc2fa31a21974cd6a75480267d5903c4e9a8c97
willywil <jonwilhelmjr@hotmail.com>
2019-10-31 17:45:30 -0500

    Add project files.

---
+++ Project2/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv) {
+	Mat image;
+	cout << "hello world" << endl;
+
+
+	//waitKey(2320);
+	system("Pause");
+	return 0;
+}
